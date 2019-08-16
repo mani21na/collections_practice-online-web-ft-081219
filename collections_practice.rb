a9e74a7e22fa8dc3f1507ce8bd570d876d731126
Hanna Lee <mani21na@gmail.com>
2019-08-16 21:04:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,7 +39,10 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.collect{|word| word << "s"}
+  array.each_with_index.collect do |word, index|
+    if index != 1 
+      word << "s"
+    end
 end
 
 
