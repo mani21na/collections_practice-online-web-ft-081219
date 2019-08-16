3626198544168c607efa932768ca2e514a1a6955
Hanna Lee <mani21na@gmail.com>
2019-08-16 20:48:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array_integers)
+  array_integers.sort
+end
 
+def sort_array_desc(array_integers)
+  array_integers.sort{|a, b| b <=> a}
+end
+
+def sort_array_char_count(array_strings)
+  array_strings.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1],array[2] = array[2],array[1]
+  array
+end
+
+#def swap_elements_from_to(array, num1, num2)
+#  array[num1],array[num2] = array[num2],array[num1] 
+#  array
+#end
+
+def reverse_array(array_integers)
+  array_integers.reverse
+end
+
+def kesha_maker(array_strings)
+  new_array = []
+  array_strings.each do |word|
+    new_array <<  word.to_a[2] = "$"
+  end
+  new_array
+end
